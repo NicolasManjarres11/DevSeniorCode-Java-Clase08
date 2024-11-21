cbdaf096e4c48f846d95ca34b6c1b8ec1545f2f7
Nicolas Manjarres Gonzalez <111820968+NicolasManjarres11@users.noreply.github.com>
2024-11-21 03:17:43 +0000

    prueba github

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("It's time to work!");
+        System.out.println("Print de prueba");
+        
     }
 }
