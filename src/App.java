74b0690a8794c66240780c06e27cbc4d0d89514f
Nicolas Manjarres Gonzalez <nicolasmanjarresg@gmail.com>
2024-11-20 22:57:11 -0500

    Nueva funcion

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("It's time to work!");
         System.out.println("Print de prueba");
+        System.out.println("Nueva funcion");
         
     }
 }
